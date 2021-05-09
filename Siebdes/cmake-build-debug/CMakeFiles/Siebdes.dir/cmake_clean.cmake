bb5628eb65553f0358181e1072b62ff43fa3f4d4
Noah <nkempers@outlook.de>
2021-05-09 12:08:30 +0200

    Initial commit

---
+++ Siebdes/cmake-build-debug/CMakeFiles/Siebdes.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Siebdes.dir/main.cpp.o"
+  "Siebdes"
+  "Siebdes.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Siebdes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Siebdes/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a[10000];
+    for (int i = 0; i <= 10000; i++){
+        a[i]=i+1;
+    }
+    for (int i = 0; i <= 10000; i++){
+        if(a[i]!=1 && a[i] != 2){
+            int b = a[i]%2;
+            if(b==0){
+                a[i]=0;
+            }
+        }
+    }
+    for (int i = 2; i <= 10000; i++){
+        if(a[i] != 0){
+            for (int j = 3;j <= 10000; j++){
+                if(a[i] != j){
+                    int b = a[i]%j;
+                    if(b == 0){
+                        a[i] = 0;
+                    }
+                }
+            }
+        }
+    }
+    for (int i = 1; i <= 10000; i++){
+        if(a[i-1] != 0){
+            cout << a[i-1] << "\n";
+        }
+    }
+}
